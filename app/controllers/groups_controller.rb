67786081920dd684a875c898dd583921a378729a
zengzheng <zengzheng2009@gmail.com>
2016-12-13 14:53:57 +0800

    implement groups#destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,4 +6,32 @@ class GroupsController < ApplicationController
     @group = Group.new
   end
 
+  def create
+    @group = Group.new(group_params)
+    @group.save
+     redirect_to groups_path
+   end
+
+   def edit
+     @group = Group.find(params[:id])
+   end
+
+   def update
+     @group = Group.find(params[:id])
+     @group.update(group_params)
+     redirect_to groups_path, notice: "Update Success"
+   end
+
+   def destroy
+     @group = Group.find(params[:id])
+     @group.destroy
+     redirect_to groups_path, notice: "Group deleted"
+   end 
+
+   private
+
+   def group_params
+     params.require(:group).permit(:title, :description)
+   end
+
 end
