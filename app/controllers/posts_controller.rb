6c88f3be47a572e321020adafbeb7c7e5ec5bde1
zengzheng <zengzheng2009@gmail.com>
2016-12-14 17:52:42 +0800

    implement post create function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,26 @@
 class PostsController < ApplicationController
+
+  def new
+    @group = Group.find(params[:group_id])
+    @post = Post.new
+  end
+
+  def create
+    @group = Group.find(params[:group_id])
+    @post = Post.new(post_params)
+    @post.group = @group #对象赋予给对象，可以理解为@post.user_id = current_user.id 值赋予值id
+    @post.user = current_user #对象赋予给对象，可以理解为@post.group_id = @group.id 值赋予值id
+
+    if @post.save
+      redirect_to group_path(@group)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
