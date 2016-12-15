d01582dc8cf5ea8ea56d5eef95578971e12a1d5e
zengzheng <zengzheng2009@gmail.com>
2016-12-15 16:02:13 +0800

    join or quit group

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,12 @@ class User < ApplicationRecord
   def is_member_of?(group) #(group)是先传一个值给它，然后判断它是否是该成员。
     participated_groups.include?(group)
   end
+
+  def join!(group)
+    participated_groups << group # << 可以理解为[1, 2]<< [3] 得出 [1, 2, 3]
+  end
+
+  def quit!(group)
+    participated_groups.delete(group)
+  end
 end
