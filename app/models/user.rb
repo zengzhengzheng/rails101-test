11e10cbd98a42ae890e83617cde0ba380b06f8ab
zengzheng <zengzheng2009@gmail.com>
2016-12-15 15:48:53 +0800

    implement is_member_of?(group)

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :group_relationships
   has_many :participated_groups, :through => :group_relationships, :source => :group
+
+  def is_member_of?(group) #(group)是先传一个值给它，然后判断它是否是该成员。
+    participated_groups.include?(group)
+  end
 end
