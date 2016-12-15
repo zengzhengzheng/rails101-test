50193d472efe85093cdb35d1472ebe9995c2f401
zengzheng <zengzheng2009@gmail.com>
2016-12-15 22:40:22 +0800

    implement my post

--- app/controllers/account/groups_controller.rb
+++ app/controllers/account/groups_controller.rb
@@ -2,6 +2,6 @@ class Account::GroupsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @groups = current_user.paticipated_groups
+    @groups = current_user.participated_groups
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,19 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to account_posts_path
+  end
+
+
   private
 
   def post_params
